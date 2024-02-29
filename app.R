ebbde51b14d74126bf56de0db489a304b0efc36f
Philipp Thomann <philipp.thomann@d-one.ai>
2024-02-29 09:43:39 +0100

    reworking the UI

--- app.R
+++ app.R
@@ -3,11 +3,14 @@ library(gridlayout)
 library(bslib)
 library(IPDfromKM)
 library(rms)
+library(shinydashboard)
+# using survminer https://rpkgs.datanovia.com/survminer/
+library(survminer)
 # library(plotly)
 
 getOriginal <- function(){
     
-  ## First step, go on digitilzest and for each arm (important, use 100 as the maximum in y axis), you export an csv files on your Desktop (EXP for the experimental arm, and CON for the control arm)
+  ## First step, go on digitilzese and for each arm (important, use 100 as the maximum in y axis), you export an csv files on your Desktop (EXP for the experimental arm, and CON for the control arm)
   
   ## first the experimental arm
   ##read the data
@@ -143,86 +146,88 @@ newData <- function(startTime=0,
   return( list(xyz=list(x=x,y=y,z=z)))
 }
 
-ui <- grid_page(
-  layout = c(
-    "header  header  ",
-    "sidebar plot "
-  ),
-  row_sizes = c(
-    "100px",
-    "1fr"
-  ),
-  col_sizes = c(
-    "250px",
-    "1fr"
-  ),
-  gap_size = "1rem",
-  grid_card(
-    area = "sidebar",
-    card_header("Settings"),
-    card_body(
-      selectInput(
-        inputId = "trial",
-        label = "Trial",
-        choices = list(
-          "CONTACT-02-PFS" = "CONTACT-02-PFS",
-          "CONTACT-02-OS" = "CONTACT-02-OS"
-        ),
-        selected = "CONTACT-02-PFS",
-        width = "100%"
-      ),
-      em("Select the trial you want to use."),
-      markdown(
-        mds = c(
-          "**Novel Abstraction**"
-        )
-      ),
-      sliderInput(
-        inputId = "time_start",
-        label = "Start Time",
-        min = 0,
-        max = ceiling(max(original$xyz$x)),
-        value = 0,
-        width = "100%"
-      ),
-      sliderInput(
-        inputId = "time_end",
-        label = "End Time",
-        min = 0,
-        max = ceiling(max(original$xyz$x)),
-        value = 3,
-        width = "100%"
-      ),
-      numericInput(
-        inputId = "Eperc",
-        label = "Eperc",
-        value = 15,
-        max = 100
-      ),
-      numericInput(
-        inputId = "Cperc",
-        label = "Cperc",
-        value = 15,
-        max = 100
-      )
-    )
-  ),
-  grid_card_text(
-    area = "header",
-    content = "A new type of informative censoring",
-    alignment = "start",
-    is_title = FALSE
-  ),
-  grid_card(
-    area = "plot",
-    card_header("KM Curve"),
-    card_body(
-      plotOutput(outputId = "plot"),
-      verbatimTextOutput(outputId = "statistics")
-    )
-  )
-)
+# Define UI - version 0004
 
+# Adding an image - code from Maria
+# text_disclaimer_outdooractive = 
+#   c("<a href='//www.outdooractive.com' title='This website uses technology and content from the Outdooractive Platform.'>
+#     <img src='//res.oastatic.com/partner/outdooractive-black.png' alt='outdooractive' />
+#     This website uses technology and content from the Outdooractive Platform.</a>",
+
+ui <- fluidPage(
+
+  titlePanel("Breaking ICE"),
+
+      box(
+        title="Trial Data",  br(),
+        selectInput(
+          inputId = "cut",
+          label = "Select a trial",
+          choices = list(
+            "CONTACT-02-PFS" = "CONTACT-02-PFS",
+            "CONTACT-02-OS" = "CONTACT-02-OS"
+          ),
+          selected = "CONTACT-02-PFS",
+          width = "100%"
+        ),
+        textInput("text", "Or load your own trial data:")
+      )
+        ,
+
+        sidebarLayout(
+
+          position = c("right"),
+          #fluid = TRUE,
+
+        sidebarPanel(
+          h3("Settings"),   "Change the parameters to see how it affects the survival analysis on the left",br(),
+          sliderInput(
+            inputId = "time_start",
+            label = "Start Time",
+            min = 0,
+            max = ceiling(max(original$xyz$x)),
+            value = 0,
+            width = "100%"
+          ),
+          sliderInput(
+            inputId = "time_end",
+            label = "End Time",
+            min = 0,
+            max = ceiling(max(original$xyz$x)),
+            value = 3,
+            width = "100%"
+          ),
+          numericInput(
+            inputId = "Eperc",
+            label = "Eperc",
+            value = 15,
+            max = 100
+          ),
+          numericInput(
+            inputId = "Cperc",
+            label = "Cperc",
+            value = 15,
+            max = 100
+          )
+          #,
+          #textInput("text", "Text input:")
+        ),
+
+        mainPanel(
+          h3("KM curve"),
+          plotOutput("plot"),
+          # verbatimTextOutput("statistics")
+          box(
+            title="Results",  br(),
+            "exp(z):", textOutput("statistics_z"),
+            "p-level:", textOutput("statistics_plevel"),
+            "interval:", textOutput("statistics_interval")
+            # textInput("text", "Or load your own trial data:")
+          )
+        ),
+
+      ),
+)
 
 server <- function(input, output) {
   
@@ -232,36 +237,88 @@ server <- function(input, output) {
   newFit <- reactive({
     survfit(Surv(x,y)~z, data=newXYZ())
   })
-  
+  newStatistics <- reactive({
+    fit <- coxph(Surv(x,y)~z, data=newXYZ())
+    s <- summary(fit)
+    s
+  })
+
+  # output$plot <- renderPlot({
+  #   ##then Kaplan-Meier Curve
+  #   # par(mar = c(1, 1, 1, 1), xaxs = "i", yaxs = "i")
+  #   plot(originalFit, xlim = c(0, 30), ylim = c(0, 1),
+  #        col=c("#66CC66", "#60C060"), lwd=3, xaxt='n', bty = "l",
+  #        las = 1, cex.axis = 1.5, tcl  = -0.5)
+  #   axis(side=1, at=seq(0, 30, by=3), cex.axis = 1.5)
+  # 
+  #   ##then Kaplan-Meier Curve
+  #   # par(mar = c(1, 1, 1, 1), xaxs = "i", yaxs = "i")
+  #   lines(newFit(), xlim = c(0, 30), ylim = c(0, 1),
+  #        col=c("red", "#6699CC"), lwd=3, xaxt='n', bty = "l",
+  #        las = 1, cex.axis = 1.5, tcl  = -0.5)
+  #   # axis(side=1, at=seq(0, 30, by=3), cex.axis = 1.5)
+  #   abline(h = 0, v = 0)
+  # 
+  # 
+  #   ## then Cox, HR and p-value
+  #   # summary(coxph(Surv(x,y)~z))
+  # 
+  #   # legend
+  #   legend("topright", inset=.02,
+  #          legend=c("Original","Original", "Docetaxel", "Sotorasib"),
+  #          col=c("#66CC66", "#60C060","red", "#6699CC"),
+  #          title="Group",
+  #          box.lty=0, lty=1:2, cex=0.8)
+  # 
+  # 
+  # })
+
+
+  newStatistics <- reactive({
+    fit <- coxph(Surv(x,y)~z, data=newXYZ())
+    s <- summary(fit)
+    list(
+      z=s$coefficients[2],
+      plevel=s$coefficients[5],
+      interval=s$conf.int[3:4]
+    )
+  })
+  output$statistics_z <- renderText({newStatistics()$z})
+  output$statistics_plevel <- renderText({newStatistics()$plevel})
+  output$statistics_interval <- renderText({newStatistics()$interval})
+
   output$plot <- renderPlot({
-    ##then Kaplan-Meier Curve
-    # par(mar = c(1, 1, 1, 1), xaxs = "i", yaxs = "i")
-    plot(originalFit, xlim = c(0, 30), ylim = c(0, 1), 
-         col=c("#66CC66", "#60C060"), lwd=3, xaxt='n', bty = "l", 
-         las = 1, cex.axis = 1.5, tcl  = -0.5)
-    axis(side=1, at=seq(0, 30, by=3), cex.axis = 1.5)
     
-    ##then Kaplan-Meier Curve
-    # par(mar = c(1, 1, 1, 1), xaxs = "i", yaxs = "i")
-    lines(newFit(), xlim = c(0, 30), ylim = c(0, 1), 
-         col=c("red", "#6699CC"), lwd=3, xaxt='n', bty = "l", 
-         las = 1, cex.axis = 1.5, tcl  = -0.5)
-    # axis(side=1, at=seq(0, 30, by=3), cex.axis = 1.5)
-    abline(h = 0, v = 0)
-    ## then Cox, HR and p-value
-    
-    
-    ## then Cox, HR and p-value
-    # summary(coxph(Surv(x,y)~z))
+    fit <- survfit(Surv(x,y)~z, data = original$xyz)
+
+    # ggsurvplot(fit, data = original$xyz)
+
+
+  g <- ggsurvplot(
+    fit,
+    data = original$xyz,
+    size = 1,                 # change line size
+    palette =
+      c("red", "#6699CC"),    # custom color palettes
+    conf.int = FALSE,          # Add confidence interval
+    pval = TRUE,              # Add p-value
+    risk.table = TRUE,        # Add risk table
+    risk.table.col = "strata",  # Risk table color by groups
+    legend.labs =
+      c("Docetaxel - Control", "Sotorasib - Experiment"),
+    # Change legend labels
+    risk.table.height = 0.25, # Useful to change when you have multiple groups
+    xlab = "Time in months",   # customize X axis label.
+    break.time.by = 3,     # break X axis in time intervals by 3.
+    ggtheme = theme_bw(),      # Change ggplot2 theme theme_light(), 
+    ncensor.plot = FALSE,      # plot the number of censored subjects at time t
+    ncensor.plot.height = 0.25
+  )
+  print(class(g))
+  print(g)
+
   })
-    
-  output$statistics <- renderPrint({
-    s <- summary(coxph(Surv(x,y)~z, data=newXYZ()))
-    print(s)
-  })
-  
-  # output$
-  
+
 }
 
 shinyApp(ui, server)
